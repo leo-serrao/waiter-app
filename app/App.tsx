62eb64bf81a74d0178c82400fe8adad062a3a97d
Leonardo Serrão <leoserraom@gmail.com>
2023-04-04 23:45:17 -0300

    formating tools and main page configured

--- app/App.tsx
+++ app/App.tsx
@@ -1,33 +1,22 @@
 import { useFonts } from 'expo-font';
+import { Main } from './src/Main';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [isFontsLoaded] = useFonts({
+  const [FontsLoaded] = useFonts({
     GeneralSans400: require('./src/assets/fonts/GeneralSans-Regular.otf'),
-    GeneralSans500: require('./src/assets/fonts/GeneralSans-Semibold.otf'),
-    GeneralSans600: require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    GeneralSans600: require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    GeneralSans700: require('./src/assets/fonts/GeneralSans-Bold.otf'),
   });
 
-  if (!isFontsLoaded) {
+  if (!FontsLoaded) {
     return null;
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'GeneralSans400' }}>HELLO World!</Text>
-      <Text style={{ fontFamily: 'GeneralSans500' }}>HELLO World!</Text>
-      <Text style={{ fontFamily: 'GeneralSans600' }}>HELLO World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='dark' />
+      <Main />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
