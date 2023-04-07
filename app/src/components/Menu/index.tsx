310f4cf9dcd17da3557bb92fb4447fb11cf0017c
Leonardo Serrão <leoserraom@gmail.com>
2023-04-07 18:55:57 -0300

    menu in production

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,3 +1,31 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+import { Product, ProductDetails, ProductImage, Separator } from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      ItemSeparatorComponent={Separator}
+      keyExtractor={product => product._id}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage source={{
+            uri: `http://192.168.0.111:3001/uploads/${product.imagePath}`
+          }} />
+          <ProductDetails>
+            <Text weight='600'>{product.name}</Text>
+            <Text color='#666' size={14} style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight='600'>{formatCurrency(product.price)}</Text>
+          </ProductDetails>
+        </Product>
+      )}
+    />
+  );
 }
