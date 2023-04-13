9cb9eb5fe17456792f7a48ac62965e8d3269a1bc
Leonardo Serrão <leoserraom@gmail.com>
2023-04-13 00:26:11 -0300

    Starting Cart component and Cart list finished

--- app/src/components/Cart/index.tsx
+++ app/src/components/Cart/index.tsx
@@ -1,8 +1,21 @@
-import { FlatList } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native';
 
 import { CartItem } from '../../types/CartItem';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { Button } from '../Button';
+import { MinusCircle } from '../Icons/MinusCircle';
+import { PlusCircle } from '../Icons/PlusCircle';
 import { Text } from '../Text';
-import { Actions, Image, Item, ProductContainer } from './syles';
+import {
+  Actions,
+  Image,
+  Item,
+  ProductContainer,
+  ProductDetails,
+  QuantityContainer,
+  Summary,
+  TotalContainer
+} from './syles';
 
 interface CartProps {
   cartItems: CartItem[]
@@ -10,24 +23,53 @@ interface CartProps {
 
 export function Cart({ cartItems }: CartProps) {
   return (
-    <FlatList
-      data={cartItems}
-      keyExtractor={cartItem => cartItem.product._id}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item: cartItem }) => (
-        <Item>
-          <ProductContainer>
-            <Image
-              source={{
-                uri: `http://192.168.0.111:3001/uploads/${cartItem.product.imagePath}`
-              }}
-            />
-          </ProductContainer>
+    <>
+      <FlatList
+        data={cartItems}
+        keyExtractor={cartItem => cartItem.product._id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item: cartItem }) => (
+          <Item>
+            <ProductContainer>
+              <Image
+                source={{
+                  uri: `http://192.168.0.111:3001/uploads/${cartItem.product.imagePath}`
+                }}
+              />
 
-          <Actions>
-          </Actions>
-        </Item>
-      )}
-    />
+              <QuantityContainer>
+                <Text size={14} color='#666'>x{cartItem.quantity}</Text>
+              </QuantityContainer>
+
+              <ProductDetails>
+                <Text size={14} weight='600'>{cartItem.product.name}</Text>
+                <Text size={14} color='#666' style={{ marginTop: 4 }}>
+                  {formatCurrency(cartItem.product.price)}
+                </Text>
+              </ProductDetails>
+            </ProductContainer>
+
+            <Actions>
+              <TouchableOpacity>
+                <PlusCircle />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ marginLeft: 24 }}>
+                <MinusCircle />
+              </TouchableOpacity>
+            </Actions>
+          </Item>
+        )}
+      />
+
+      <Summary>
+        <TotalContainer>
+          <Text color='#666'>Total</Text>
+          <Text size={20} weight='600'>{formatCurrency(120)}</Text>
+        </TotalContainer>
+
+        <Button onPress={() => null} />
+      </Summary>
+    </>
   );
 }
--- app/src/components/Cart/syles.ts
+++ app/src/components/Cart/syles.ts
@@ -2,9 +2,14 @@ import styled from 'styled-components/native';
 
 export const Item = styled.View`
   padding: 8px 0;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-export const ProductContainer = styled.View``;
+export const ProductContainer = styled.View`
+  flex-direction: row;
+`;
 
 export const Image = styled.Image`
   width: 48px;
@@ -12,4 +17,17 @@ export const Image = styled.Image`
   border-radius: 6px;
 `;
 
-export const Actions = styled.View``;
+export const QuantityContainer = styled.View`
+  min-width: 20px;
+  margin-left: 12px;
+`;
+
+export const ProductDetails = styled.View``;
+
+export const Actions = styled.View`
+  flex-direction: row;
+`;
+
+export const Summary = styled.View``;
+
+export const TotalContainer = styled.View``;
